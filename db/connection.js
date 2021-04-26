9d48f218f5b4754b5357dff0eda3afb5a26565ba
bob <robertbalonek@gmail.com>
2021-04-26 15:22:08 -0700

    changed database name

--- db/connection.js
+++ db/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 let MONGODB_URI =
   process.env.PROD_MONGODB ||
-  "mongodb+srv://Bob:Bob@cluster0.tpvhu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+  "mongodb+srv://Bob:Bob@cluster0.tpvhu.mongodb.net/mernProjPortfolio?retryWrites=true&w=majority";
 mongoose
   .connect(MONGODB_URI, { useUnifiedTopology: true, useNewUrlParser: true })
   .then(() => console.log("Successfully connected to MongoDB."))
