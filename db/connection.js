bae43f2dbf5a220c75c849d6528294c7a136f8e2
bob <robertbalonek@gmail.com>
2021-04-26 15:12:48 -0700

    Seeded

--- db/connection.js
+++ db/connection.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 let MONGODB_URI =
-  process.env.PROD_MONGODB || "mongodb://127.0.0.1:27017/blogPosts";
+  process.env.PROD_MONGODB ||
+  "mongodb+srv://Bob:Bob@cluster0.tpvhu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 mongoose
   .connect(MONGODB_URI, { useUnifiedTopology: true, useNewUrlParser: true })
   .then(() => console.log("Successfully connected to MongoDB."))
