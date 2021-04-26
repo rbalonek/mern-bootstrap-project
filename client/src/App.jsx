3ee1ff0869c7a780d76031600e002e1d8c62f063
bob <robertbalonek@gmail.com>
2021-04-26 15:56:19 -0700

    Layout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,14 @@
-import logo from "./logo.svg";
+import React from "react";
 import "./App.css";
+import Home from "./screens/Home/Home";
+import { Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hi</h1>
+      <Switch>
+        <Route exact path="/" component={Home} />
+      </Switch>
     </div>
   );
 }
