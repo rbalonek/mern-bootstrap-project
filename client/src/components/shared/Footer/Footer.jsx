74364da15351950840acbcb861247a960b38b43d
bob <robertbalonek@gmail.com>
2021-04-27 12:46:07 -0700

    Changed Name

--- client/src/components/shared/Footer/Footer.jsx
+++ client/src/components/shared/Footer/Footer.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const Footer = () => (
   <footer style={{ position: "fixed", bottom: "0" }}>
-    <nav className="navbar navbar-dark bg-dark vw-100 p-1">
+    <nav className="navbar navbar-dark bg-dark vw-100 p-1 ">
       <div className=" w-100 d-flex justify-content-center">
-        <p className=" text-secondary font-weight-light mt-3">
+        <p className=" text-secondary font-weight-light mt-3  ">
           Created by : Robert Balonek for a MERN / Bootstrap exercise{" "}
         </p>
       </div>
