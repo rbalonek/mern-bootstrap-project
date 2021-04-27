4401a6a64436c914296036dce70b0ad7ecbace90
bob <robertbalonek@gmail.com>
2021-04-26 20:11:08 -0700

    flexing

--- client/src/screens/ProductDetail/ProductDetail.jsx
+++ client/src/screens/ProductDetail/ProductDetail.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import Layout from "../../components/shared/Layout/Layout";
 import { getProduct, deleteProduct } from "../../services/products";
 import { useParams, Link, useHistory } from "react-router-dom";
+import { Image } from "react-bootstrap";
 
 const ProductDetail = (props) => {
   const history = useHistory();
@@ -28,55 +29,67 @@ const ProductDetail = (props) => {
 
   return (
     <Layout>
-      <div style={{ marginTop: "50px" }}>
-        <img
-          style={{ height: "100px" }}
-          src={product.imgURL}
-          alt={product.name}
-        />
+      <div
+      // style={{ marginTop: "50px" }}
+      >
         <div>
-          <div>{product.name}</div>
-          <div>{`$${product.price}`}</div>
-          <div>{product.description}</div>
-          <div>
-            {loggedIn ? (
-              <>
-                <button className="btn btn-warning">
-                  <Link
-                    className="edit-link"
-                    to={`/products/${product._id}/edit`}
+          <div className="d-flex flex-row">
+            <Image
+              fluid
+              rounded
+              style={{ height: "150px" }}
+              src={product.imgURL}
+              alt={product.name}
+            />
+            <div>{product.name}</div>
+            <div>{`$${product.price}`}</div>
+          </div>
+
+          <div className="d-flex flex-column">
+            <div>{product.description}</div>
+            <div>
+              {loggedIn ? (
+                <>
+                  <button className="btn btn-warning">
+                    <Link
+                      className="edit-link"
+                      to={`/products/${product._id}/edit`}
+                    >
+                      Edit
+                    </Link>
+                  </button>
+                  <button
+                    className="btn btn-danger m-3"
+                    onClick={() => {
+                      deleteProduct(product._id);
+                      history.push("/products");
+                      alert(`Item ${product.name} Deleted!`);
+                    }}
                   >
-                    Edit
-                  </Link>
-                </button>
-                <button
-                  className="btn btn-danger m-3"
-                  onClick={() => {
-                    deleteProduct(product._id);
-                    history.push("/products");
-                    alert(`Item ${product.name} Deleted!`);
-                  }}
-                >
-                  Delete
-                </button>
-                <button
-                  className="btn btn-primary"
-                  onClick={props.handleChangeLogout}
-                >
-                  Logout
-                </button>
-              </>
-            ) : (
-              <>
-                <button className="btn btn-warning disabled"> Purchase </button>
-                <button
-                  className="btn btn-primary m-3"
-                  onClick={props.handleChangeLogin}
-                >
-                  Login
-                </button>
-              </>
-            )}
+                    Delete
+                  </button>
+                  <button
+                    className="btn btn-primary"
+                    onClick={props.handleChangeLogout}
+                  >
+                    Logout
+                  </button>
+                </>
+              ) : (
+                <>
+                  <button className="btn btn-warning disabled">
+                    {" "}
+                    Purchase{" "}
+                  </button>
+                  <button
+                    className="btn btn-primary m-3"
+                    onClick={props.handleChangeLogin}
+                  >
+                    Login
+                  </button>
+                </>
+              )}
+            </div>
           </div>
         </div>
       </div>
